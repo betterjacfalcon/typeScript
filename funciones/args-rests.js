4d6f6ee44f343e1e15698653ca0c2d224d972c55
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-12 18:54:10 -0600

    Parametros por rest en la funcion

---
+++ funciones/args-rests.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const fullName = (firstName, ...restArgs) => {
+        return `${firstName} ${restArgs.join(' ')} `;
+    };
+    const superman = fullName('clark', 'Joseph', 'Kent');
+    console.log({ superman });
+})();
