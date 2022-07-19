a0b71fd011cd8fbbd57328ac852bfb5b0bc9b2e4
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-18 21:27:20 -0600

    Objecto con la misma estructura

--- objetos/objects.js
+++ objetos/objects.js
@@ -5,14 +5,20 @@
         age: 24,
         power: ['Super Velocidad', 'Viajar en el tiempo']
     };
+
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        power: ['Super Fuerza']
+    };
     /*Objero basico todo lo que esta definido aqui debe estar en la
-    //Declaracion de let frash */
+    //Declaracion de let frash 
     flash = {
         name: 'Clark Kent',
         power: ['Super Fuerza'],
         getName() {
             return this.name;
         }
-    };
-    console.log(flash);
+    }; 
+    console.log(flash); */
 })();
