a9738ad48efbc645e2f032b2b5a67ec88e8dd601
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-18 21:00:22 -0600

    Objecto opcionales

--- objetos/objects.js
+++ objetos/objects.js
@@ -6,10 +6,10 @@
         power: ['Super Velocidad', 'Viajar en el tiempo']
     };
     /*Objero basico todo lo que esta definido aqui debe estar en la
-    //Declaracion de let frash*/
+    //Declaracion de let frash */
     flash = {
         name: 'Clark Kent',
-        age: 60,
         power: ['Super Fuerza']
     };
+    console.log(flash);
 })();
--- objetos/objects.ts
+++ objetos/objects.ts
@@ -1,17 +1,18 @@
 (( ) => {
     
-    let flash = {
+    let flash: {name:string, age?:number, power:string[]} = {
         name: 'Barry Allen',
         age: 24,
         power: ['Super Velocidad', 'Viajar en el tiempo']
     } 
 
     /*Objero basico todo lo que esta definido aqui debe estar en la 
-    //Declaracion de let frash*/
+    //Declaracion de let frash */
     flash = {
-        name: 'Clark Kent',
-        age: 60,
+        name: 'Clark Kent',                
         power: ['Super Fuerza']
-    } 
+    }
+
+    console.log(flash);
 
 })( )
