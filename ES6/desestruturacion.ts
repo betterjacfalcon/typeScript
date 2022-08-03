c05315ad0630c3a0edb677a4462cb06e31214e5f
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-08-03 06:00:45 -0600

    Destructuracion de Objectos

---
+++ ES6/desestruturacion.ts
@@ -0,0 +1,29 @@
+(() => {
+
+    type Heroes = {
+        nick: string;
+        iroman: string;
+        vision: string;
+        activo: boolean;
+        poder: number;
+    }
+
+    const heroes:Heroes = {
+        nick: 'Samuel Jackson',
+        iroman: 'Robert Downey',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500.111
+    }
+
+    /*const{poder, vision}=heroes;
+    console.log(poder.toFixed(2), vision.toUpperCase());*/
+
+    const printHeroes = ({nick, ...resto}:Heroes
+    ) => {
+        console.log(nick, resto);
+    }
+
+    printHeroes(heroes);
+
+})()
---
+++ ES6/let-variable.ts
@@ -0,0 +1,8 @@
+(( ) => {
+
+    const nombre:string = 'alejandra';
+
+    const getName = ():void => {
+        console.log('viejo get name');
+    }
+})( )
